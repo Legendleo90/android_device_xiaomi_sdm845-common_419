1af20c4a972183f89658bb1681679b396c1a8ea7
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-29 16:43:55 +0100

    sdm845-common: Link Dolby Atmos dependencies against v33 libstagefright_foundation
    * fixes crashes and absurdly high CPU usage while using dolby
    
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Co-Authored-By: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -53,6 +53,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('gettid: 1'),
     ('vendor/lib/hw/audio.primary.sdm845.so', 'vendor/lib/hw/sound_trigger.primary.sdm845.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    ('vendor/lib64/libdlbdsservice.so', 'vendor/lib/libstagefright_soft_ac4dec.so', 'vendor/lib/libstagefright_soft_ddpdec.so'): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
