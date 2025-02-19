cc4e773ef0998f885dff693805e06e65035849b1
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sdm845-common: Build common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- sdm845.mk
+++ sdm845.mk
@@ -109,6 +109,11 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
