d6b000fd4c0cf7aabad9e3b3fa2bf35646f4ad62
Akshat <Legendleo90@users.noreply.github.com>
2025-12-23 11:17:55 +0530

    sdm845-common: Drop livedisplay for now

--- sdm845.mk
+++ sdm845.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libstagefright_foundation-v33 \
