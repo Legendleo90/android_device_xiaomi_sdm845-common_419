0d856752e308e31c921e949c456592021116f5a2
K R I S H N A <krishnakripa34567@gmail.com>
2026-01-28 06:06:43 +0000

    sdm845-common: Inherit vendor/sony/dolby
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from Dolby
+$(call inherit-product-if-exists, vendor/sony/dolby/sonydolby.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
